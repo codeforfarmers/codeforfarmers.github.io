886fcfa3fb61bc704784f997ac982fc6efa3caca
leehas <sahmed@qwinixtech.com>
2015-04-01 16:20:38 +0530

    Fixed email sending part

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,3 +1,30 @@
+/* http://www.smartaddon.com/contact_form.html */
+/*var sa_email_id = '48487-a6e46';  // saheelahmed7@gmail.com
+var sa_email_id = '48483-821b8'; // sahmed@qwinixtech.com*/ 
+var sa_email_id = '48498-3eab3'; /*dputtannaiah@qwinixtech.com */
+var sa_sent_text = 'Thank you for contacting us. We will get back to you soon.';
+
+var sa_params = '';
+var sa_frm = document.sentMessage;
+			
+function sa_htmlent(_val) {
+	return _val;
+}
+
+function sa_contactsent() {
+	// Success message
+    $('#success').html("<div class='alert alert-success'>");
+    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+    $('#success > .alert-success')
+        .append("<strong>"+ sa_sent_text +"</strong>");
+    $('#success > .alert-success')
+        .append('</div>');
+
+    //clear all fields
+    $('#contactForm').trigger("reset");
+}
+
 $(function() {
 
     $("input,textarea").jqBootstrapValidation({
@@ -8,49 +35,17 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+			for (i = 0; i < sa_frm.elements.length; i++) {
+				var sa_el = sa_frm.elements[i];
+				if (sa_frm.elements[i].name) {
+					sa_params += '<input type=hidden name=' + sa_frm.elements[i].name + ' value="' + sa_frm.elements[i].value + '">';
+				}
+			}
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+			var s = document.createElement('script');
+			s.setAttribute('type', 'text/javascript');
+			s.setAttribute('src', 'http://www.smartaddon.com/js/postform.js');
+			document.body.appendChild(s);
         },
         filter: function() {
             return $(this).is(":visible");
